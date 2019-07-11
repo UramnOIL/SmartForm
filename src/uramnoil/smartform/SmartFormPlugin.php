7748bcb9205b9de25186f48d5898bcfd8c73524e
uramnoil <uramnoil@outlook.jp>
2019-07-12 00:39:54 +0900

    SmartFormPluginクラスにISmartFormManagerインターフェースの実装とgetSmartFormManager関数を作成

--- src/uramnoil/smartform/SmartFormPlugin.php
+++ src/uramnoil/smartform/SmartFormPlugin.php
@@ -4,7 +4,22 @@
 namespace uramnoil\smartform;
 
 
-class SmartFormPlugin
-{
+use pocketmine\form\Form;
+use pocketmine\plugin\PluginBase;
 
+class SmartFormPlugin extends PluginBase implements ISmartFormManager {
+	/** @var Form[] */
+	private iterable $forms = [];
+
+	public function getSmartFormManager() : ISmartFormManager {
+		return $this;
+	}
+
+	public function createSmartForm(): Form {
+
+	}
+
+	public function registerFactory(IFormFactory $factory) : void {
+		$form[$factory->getName()] = $factory;
+	}
 }
