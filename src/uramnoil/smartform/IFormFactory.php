ee6c22aa8b68ded6b31d5dd676da31725927e06e
uramnoil <uramnoil@outlook.jp>
2019-07-12 01:19:45 +0900

    getImageの返り値の型を修正

--- src/uramnoil/smartform/IFormFactory.php
+++ src/uramnoil/smartform/IFormFactory.php
@@ -4,11 +4,12 @@
 namespace uramnoil\smartform;
 
 
+use Frago9876543210\EasyForms\elements\Image;
 use pocketmine\form\Form;
 
 interface IFormFactory {
 	function createForm() : Form;
 	function getName() : string;
 	function getTitle() : string;
-	function getImage() : ?string;
+	function getImage() : ?Image;
 }
--- src/uramnoil/smartform/SmartFormPlugin.php
+++ src/uramnoil/smartform/SmartFormPlugin.php
@@ -4,29 +4,41 @@
 namespace uramnoil\smartform;
 
 
+use Frago9876543210\EasyForms\elements\Button;
 use Frago9876543210\EasyForms\forms\MenuForm;
 use pocketmine\form\Form;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
+use RuntimeException;
 use uramnoil\smartform\form\element\FormButton;
 
 class SmartFormPlugin extends PluginBase implements ISmartFormManager {
-	/** @var Form[] */
-	private $forms = [];
+	/** @var IFormFactory[] */
+	private $factories = [];
 
 	public function getSmartFormManager() : ISmartFormManager {
 		return $this;
 	}
 
 	public function createSmartForm(): Form {
-		$form = new MenuForm("SmartForm", null);
+		$form = new MenuForm("SmartForm", null, null,
+			function(Player $player, Button $selected) : void {
+				if(!$selected instanceof FormButton) {
+					throw new RuntimeException("This button is invalid.");
+				}
+				$player->sendForm($selected->getForm());
+			}
+		);
 		/** @var FormButton[] $buttons */
 		$buttons = [];
-		foreach($this->forms as $form) {
-			$buttons[] = new FormButton();
+		foreach($this->factories as $factory) {
+			$buttons[] = new FormButton($factory->getTitle(), $factory->getImage(), $factory->createForm());
 		}
+		$form->append($buttons);
+		return $form;
 	}
 
 	public function registerFactory(IFormFactory $factory) : void {
-		$form[$factory->getName()] = $factory;
+		$this->factories[$factory->getName()] = $factory;
 	}
 }
