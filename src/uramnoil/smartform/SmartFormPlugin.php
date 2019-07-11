9ccb30d2b16f34ac1f17a8b469e72f2ddf0501ad
uramnoil <uramnoil@outlook.jp>
2019-07-12 00:59:15 +0900

    FormButtonクラスに必要なメンバを生やす

--- src/uramnoil/smartform/SmartFormPlugin.php
+++ src/uramnoil/smartform/SmartFormPlugin.php
@@ -4,19 +4,26 @@
 namespace uramnoil\smartform;
 
 
+use Frago9876543210\EasyForms\forms\MenuForm;
 use pocketmine\form\Form;
 use pocketmine\plugin\PluginBase;
+use uramnoil\smartform\form\element\FormButton;
 
 class SmartFormPlugin extends PluginBase implements ISmartFormManager {
 	/** @var Form[] */
-	private iterable $forms = [];
+	private $forms = [];
 
 	public function getSmartFormManager() : ISmartFormManager {
 		return $this;
 	}
 
 	public function createSmartForm(): Form {
-
+		$form = new MenuForm("SmartForm", null);
+		/** @var FormButton[] $buttons */
+		$buttons = [];
+		foreach($this->forms as $form) {
+			$buttons[] = new FormButton();
+		}
 	}
 
 	public function registerFactory(IFormFactory $factory) : void {
