94234cc73ae522e5e09392593002a6f99cf8442c
uramnoil <uramnoil@outlook.jp>
2019-07-12 01:19:03 +0900

    不要なメンバの削除

--- src/uramnoil/smartform/form/element/FormButton.php
+++ src/uramnoil/smartform/form/element/FormButton.php
@@ -7,14 +7,18 @@ use Frago9876543210\EasyForms\elements\Image;
 use pocketmine\form\Form;
 
 class FormButton extends Button {
-	/** @var string */
-	private $name;
 	/** @var Form */
 	private $form;
 
-	public function __construct(string $name, string $text, ?Image $image, Form $form) {
-		$this->name = $name;
+	public function __construct(string $text, ?Image $image, Form $form) {
 		$this->form = $form;
 		parent::__construct($text, $image);
+	}
+
+	/**
+	 * @return Form
+	 */
+	public function getForm(): Form {
+		return $this->form;
 	}
 }
